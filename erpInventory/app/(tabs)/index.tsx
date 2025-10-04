24c5acf9dcdf032da2ffaa9d34e62bf82aef3ce3
MarlexSaravia <u21204396@utp.edu.pe>
2025-10-03 22:04:00 -0500

    test

--- erpInventory/app/(tabs)/index.tsx
+++ erpInventory/app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Main renuevo
+        <ThemedText type="title">Main renuevo...
         </ThemedText>
         <HelloWave />
       </ThemedView>
