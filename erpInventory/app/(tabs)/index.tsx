99efca6ab12f7a83a3354d6204c4a302e546bf69
MarlexSaravia <u21204396@utp.edu.pe>
2025-10-06 01:21:27 -0500

    try

--- erpInventory/app/(tabs)/index.tsx
+++ erpInventory/app/(tabs)/index.tsx
@@ -22,7 +22,7 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">porque mejorar con react native</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to xd
           Press{' '}
