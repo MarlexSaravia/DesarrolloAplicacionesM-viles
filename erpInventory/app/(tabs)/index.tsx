00701a1a5b7f5137d8ccbcda0b2c0378415d93a8
MarlexSaravia <u21204396@utp.edu.pe>
2025-10-08 22:53:53 -0500

    cc

--- erpInventory/app/(tabs)/index.tsx
+++ erpInventory/app/(tabs)/index.tsx
@@ -43,7 +43,7 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Salirse de la facut</ThemedText>
+        <ThemedText type="subtitle">Concursos agasa</ThemedText>
         <ThemedText>
           {`When you're ready, run `}
           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
