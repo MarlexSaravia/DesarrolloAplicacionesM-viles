c3c8a8d9240b3ed32b4b1738c43d93459e88edf5
MarlexSaravia <u21204396@utp.edu.pe>
2025-10-18 23:39:28 -0500

    avance 3

--- erpInventory/app/(tabs)/index.tsx
+++ erpInventory/app/(tabs)/index.tsx
@@ -37,7 +37,7 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">last change on this month</ThemedText>
+        <ThemedText type="subtitle">avance 3 de proyecto en curso</ThemedText>
         <ThemedText>
           {`Tap the Explore tab to learn more about what's included in this starter app.`}
         </ThemedText>
