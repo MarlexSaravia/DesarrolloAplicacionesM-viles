65373e01799cd79279498f877ae48ea45bb7cabc
MarlexSaravia <u21204396@utp.edu.pe>
2025-10-20 21:58:30 -0500

    delta

--- erpInventory/app/(tabs)/index.tsx
+++ erpInventory/app/(tabs)/index.tsx
@@ -43,7 +43,7 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">My HARDEST GLOWYWY</ThemedText>
+        <ThemedText type="subtitle">Sigma intergoods</ThemedText>
         <ThemedText>
           {`When you're ready, run `}
           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
