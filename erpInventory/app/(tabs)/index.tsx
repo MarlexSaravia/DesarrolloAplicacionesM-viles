1e2696ff111851d8c970aeb21f254c903bcdd71c
MarlexSaravia <u21204396@utp.edu.pe>
2025-10-29 21:48:55 -0500

    origin

--- erpInventory/app/(tabs)/index.tsx
+++ erpInventory/app/(tabs)/index.tsx
@@ -43,7 +43,7 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Proximos 4 extreme demonss</ThemedText>
+        <ThemedText type="subtitle">PZZZZs</ThemedText>
         <ThemedText>
           {`When you're ready, run `}
           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
