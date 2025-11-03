4b684bb31d2bf3260257fdbf892b3def453e9609
MarlexSaravia <u21204396@utp.edu.pe>
2025-11-02 23:40:43 -0500

    dia 2

--- erpInventory/app/(tabs)/index.tsx
+++ erpInventory/app/(tabs)/index.tsx
@@ -43,7 +43,7 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">1 de noviembre, empieza el día</ThemedText>
+        <ThemedText type="subtitle">2 de noviembre, empieza el día</ThemedText>
         <ThemedText>
           {`When you're ready, run `}
           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
